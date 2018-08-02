3240b09cce2206152deda30759e79300166c0fb9
Erik Wramner <erik.wramner@codemint.com>
2018-08-02 09:51:55 +0200

    Optionally create user and repo at startup

--- scripts/httpd-foreground.sh
+++ scripts/httpd-foreground.sh
@@ -5,14 +5,21 @@ set -e
 rm -f /usr/local/apache2/logs/httpd.pid
 
 # Ensure that we have a server name defined
-if [ "$HTTPD_SERVER_NAME" = "" ]
-then
+if [ "$HTTPD_SERVER_NAME" = "" ]; then
   HTTPD_SERVER_NAME=${HOSTNAME}; export HTTPD_SERVER_NAME
 fi
 
-if [ "$HTTPD_SERVER_ADMIN" = "" ]
-then
+if [ "$HTTPD_SERVER_ADMIN" = "" ]; then
   HTTPD_SERVER_ADMIN=admin@${HTTPD_SERVER_NAME}; export HTTPD_SERVER_ADMIN
+fi
+
+if [ ! -f /svn/config/svn-users ] && [ "$SVN_USER" != "" ] && [ "$SVN_PASSWORD" != "" ]; then
+  htpasswd -cbB /svn/config/svn-users $SVN_USER $SVN_PASSWORD
+fi
+
+if [ "$SVN_REPO_NAME" != "" ] && [ ! -d /svn/repos/$SVN_REPO_NAME ]; then
+  svnadmin create /svn/repos/$SVN_REPO_NAME
+  chown -R httpd:httpd /svn/repos/$SVN_REPO_NAME
 fi
 
 if [ "$HTTPD_SSL" = "" ]
